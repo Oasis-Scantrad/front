69e2ac87f4cec5b0658ad90a45a4fdae53f3b383
Esteban Sotillo <esteban.sotillo@olympe.ch>
2020-04-19 23:42:39 +0200

    add footer

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -12,6 +12,15 @@
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
+	footer {
+		background-color: lightgrey;
+	}
+	.sizer {
+		max-width: 56em;
+		margin: auto;
+		border-top: 3px solid #000;
+	}
+
 </style>
 
 <Nav {segment}/>
@@ -19,3 +28,15 @@
 <main>
 	<slot></slot>
 </main>
+
+<footer>
+	<div class="sizer">
+		<div class="footer-box">
+			<div class="title">Les autres equipes</div>
+			<div class="teams">
+				<a href="."><img src="https://via.placeholder.com/90x60" alt="logo-1"></a>
+				<a href="."><img src="https://via.placeholder.com/90x40" alt="logo-2"></a>
+			</div>
+		</div>
+	</div>
+</footer>
